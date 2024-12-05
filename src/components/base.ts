89f2396cfc5b88d8037020d6f14123bad1532943
Dylan Wootton <wootton.dylan@gmail.com>
2024-12-05 10:24:54 -0500

    added anchor types

--- src/components/base.ts
+++ src/components/base.ts
@@ -1,18 +1,94 @@
 import { Field } from 'vega-lite/build/src/channeldef';
 import { TopLevelSpec, UnitSpec } from 'vega-lite/build/src/spec';
 import { generateId } from '../utils/id';
-
+import { AnchorValue } from 'vega';
+import { AnchorGroupSchema, AnchorProxy, AnchorSchema } from '../types/anchors';
+export type BindingTarget = BaseComponent | AnchorProxy;
 export interface Component {
   id: string;
   type: string;
   getSpec(): any;
 }
 
-export abstract class BaseComponent {
-  id: string;
+function isComponent(obj: any): obj is BaseComponent {
+  return obj instanceof BaseComponent;
+}
 
+
+
+export abstract class BaseComponent {
+  protected anchors: Map<string, AnchorProxy> = new Map();
+  public id: string;
+  
   constructor() {
     this.id = generateId();
+    // Create special _all anchor that represents the entire component
+    this.initializeAllAnchor();
+  }
+
+  private initializeAllAnchor() {
+    const allAnchor: AnchorGroupSchema = {
+      id: '_all',
+      type: 'group',
+      // map through all anchors and add their schemas to the children
+      children: new Map(Array.from(this.anchors.entries()).map(([key, anchor]) => [key, anchor.anchorSchema]))
+    };
+    this.anchors.set('_all', this.createAnchorProxy(allAnchor));
+  }
+
+  // This is the top-level bind function that redirects to _all anchor
+  // it is only called if bind is called on a component
+  bind(target: BaseComponent | AnchorProxy): BaseComponent {
+    // If target is a component, use its _all anchor
+    const targetAnchor = isComponent(target) 
+      ? target.getAnchor('_all')
+      : target;
+
+    // Redirect binding to our _all anchor
+    return this.getAnchor('_all').bind(targetAnchor);
+  }
+
+  // Protected method to get anchor (used by bind)
+  protected getAnchor(id: string): AnchorProxy {
+    const anchor = this.anchors.get(id);
+    if (!anchor) {
+      throw new Error(`Anchor "${id}" not found`);
+    }
+    return anchor;
+  }
+
+  
+
+
+  protected createAnchorProxy(anchor: AnchorSchema): AnchorProxy {
+    // bind function that will be used if called on a specific anchor (chart.x.bind....)
+    const bindFn = (target:  BindingTarget) => {
+      
+      // If target is a component, use its _all anchor
+      const targetAnchor = isComponent(target) 
+        ? target.getAnchor('_all')
+        : target;
+
+      // Get the binding graph
+      const bindingManager = BindingManager.getInstance();
+
+      // Add the binding
+      bindingManager.addBinding(
+        this.id,targetAnchor.id.componentId, anchor.id,
+        targetAnchor.id.anchorId
+      );
+
+      //chart._all.bind(brush._all
+
+      return targetAnchor.component;
+    };
+
+    return {
+      id: { componentId: this.id, anchorId: anchor.id },
+      component: this,
+      bind: bindFn,
+      anchorSchema: anchor
+    };
   }
 
   abstract compileComponent(): Partial<UnitSpec<Field>>;
@@ -20,6 +96,40 @@ export abstract class BaseComponent {
   compile(): TopLevelSpec {
     // find the root of the component tree
     return this.compileComponent() as TopLevelSpec;
+  }
+}
+
+// Simple binding manager to track component bindings
+export class BindingManager {
+  private bindings: Array<{sourceId: string, targetId: string, sourceAnchor: string, targetAnchor: string}>;
+  private static instance: BindingManager;
+
+  private constructor() {
+    this.bindings = [];
+  }
+
+  public static getInstance(): BindingManager {
+    if (!BindingManager.instance) {
+      BindingManager.instance = new BindingManager();
+    }
+    return BindingManager.instance;
+  }
+
+  public addBinding(sourceId: string, targetId: string, sourceAnchor: string, targetAnchor: string) {
+    this.bindings.push({
+      sourceId,
+      targetId,
+      sourceAnchor,
+      targetAnchor
+    });
+  }
+
+  public getBindingsForComponent(componentId: string) {
+    return this.bindings.filter(binding => binding.sourceId === componentId || binding.targetId === componentId);
+  }
+
+  public getAllBindings() {
+    return this.bindings;
   }
 }
 
---
+++ src/types/anchors.ts
@@ -0,0 +1,45 @@
+import {  BaseComponent } from 'components/base';
+
+export interface AnchorId {
+  componentId: string;
+  anchorId: string; // TODO this needs to be randomized and cant be the actual id of the schema 
+}
+
+export type GeometricAnchorType = 'point' | 'line' | 'area' | 'path';
+export type ChannelAnchorType = 'x' | 'y' | 'x2' | 'y2' | 'color' | 'size' | 'shape';
+export type SelectionAnchorType = 'PointSelection' | 'IntervalSelection' | 'LassoSelection';
+export type OtherAnchorTypes = string;
+
+export type AnchorType = GeometricAnchorType | ChannelAnchorType | SelectionAnchorType | OtherAnchorTypes;
+
+// AnchorTypes correspond to all of the different 
+export interface AnchorId {
+  componentId: string;
+  anchorId: string; // TODO this needs to be randomized and cant be the actual id of the schema 
+}
+
+export interface AnchorSchema {
+    id: string;
+    type: AnchorType;
+}
+
+
+// Used for binding groups of anchors like the corners of a rectangle
+export interface AnchorGroupSchema {
+    id: string;
+    type: 'group';
+    children: Map<string, AnchorOrGroupSchema>;
+}
+
+export interface AnchorProxy {
+  component: BaseComponent;
+  id: AnchorId;
+  anchorSchema: AnchorOrGroupSchema;
+  bind: (target: AnchorProxy) => BaseComponent;
+}
+
+// brush.sides.top 
+// brush.sides-> get the anchor proxy for sides
+// sides.top -> get the anchor proxy for top
+  
+export type AnchorOrGroupSchema = AnchorSchema | AnchorGroupSchema;
